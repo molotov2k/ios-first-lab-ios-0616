8c4050fbc408b36b8a08677a25756d0528273ede
molotov2k <molotov2k@gmail.com>
2016-04-05 09:59:52 -0400

    my first commit

--- my-first-lab/FISAppDelegate.m
+++ my-first-lab/FISAppDelegate.m
@@ -5,14 +5,14 @@
 @implementation FISAppDelegate
 
 // //////////////////////////////////////////////////////
-// Add two slashes in front of line 24.
-// Remove two slashes from line 25.
+// Add two slashes in front of line 14.
+// Remove two slashes from line 15.
 // Press Command-U to run the tests!
 // /////////////////////////////////////////////////////
 - (NSString *)didItWork
 {
-    return nil;
-//    return @"YES";
+    // return nil;
+    return @"YES";
 }
 
 @end
